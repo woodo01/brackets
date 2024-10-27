b14c9515ad89c08de785dbb526f8652596525c85
ivan.sigaev <ivan.develop@gmail.com>
2024-10-28 00:16:12 +0100

    feat: implement brackets validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const bracketPairs = Object.fromEntries(bracketsConfig);
+
+    for (let char of str) {
+        const isOpeningBracket = bracketPairs[char] === char ? !stack.includes(char) : bracketPairs[char] !== undefined;
+
+        if (isOpeningBracket) {
+            stack.push(char);
+        } else {
+            const lastOpened = stack.pop();
+            if (bracketPairs[lastOpened] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
